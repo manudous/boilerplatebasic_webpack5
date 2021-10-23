4f3313980652f912318d9ec1e97468591e1206bf
manudous <you@example.com>
2021-10-23 16:30:38 +0200

    remove straw

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import React from "react";
-import stitch from "./content/stitch.png";
-import bambi from "./content/bambi.jpg";
-export const App = () => {
-  const [name, setName] = React.useState("");
+export const App: React.FC = () => {
   return (
     <div>
       <h1>Hello from React DOM!!!</h1>
-      <label htmlFor="name">Nombre: </label>
-      <input
-        id="name"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <img src={stitch} alt="pikachu" />
-      <img src={bambi} alt="bambi" />
     </div>
   );
 };
